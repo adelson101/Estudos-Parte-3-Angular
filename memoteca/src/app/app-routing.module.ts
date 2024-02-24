4793081cf7004cd558b65901a010358dd299d470
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-02-23 21:49:19 -0300

    update

--- memoteca/src/app/app-routing.module.ts
+++ memoteca/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListaPensamentoComponent } from './componentes/pensamentos/lista-pensamento/lista-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'listarPensamento'
+  },
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentoComponent
+  },
+  {
+    path: 'listarPensamento',
+    component: ListaPensamentoComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
